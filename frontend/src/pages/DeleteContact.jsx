5590f83c492a2b9540140b2c68c84a4837e8c0d1
Matias Tibaldo <mtibaldo@matriz.com.ar>
2020-03-12 21:49:02 -0300

    Cambios en logica de edit y delete para mejorar renderizado

--- frontend/src/pages/DeleteContact.jsx
+++ frontend/src/pages/DeleteContact.jsx
@@ -5,41 +5,58 @@ import ListContact from './ListContact';
 export default class EditContact extends React.Component   {
     state ={
         id:"",
+        deleteOn: true,
+        listContact:[],
     }
 
-    handleDeleteContact =()=>{
+    handleDeleteContact =(index)=>{
 
         axios
         .delete(`http://localhost:9000/contactAPI/delete/${this.state.id}`)
         .then(() => {
             alert("Contacto eliminado");
+            var list = this.state.listContact;
+            list.splice(index,1);
+            this.setState({
+                listContact:list,
+            })
         })
-        
         .catch(err => {
             console.error(err);
             alert("Ocurrio un error. Intente nuevamente");
         });
     }
 
-    onItemClick= (item) => {
+    onItemClick= (item, index) => {
         this.setState({
             id: `${item.name}-${item.lastName}-${item.birthDate}-${item.email}-${item.phone}`,
+            deleteOn:true,
         });
         var r = window.confirm("Esta seguro de eliminar el contacto?");
         if(r===true){
-            this.handleDeleteContact();
+            this.handleDeleteContact(index);
         }
     }
+    componentDidMount = () =>{
+        axios
+        .get('http://localhost:9000/contactAPI/list')
+        .then((res) => {
+            var list = []
+            res.data.forEach((item)=>{
+                list.push(JSON.parse(item));
+            })
+            this.setState({
+                listContact:list,
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
 
-    render(){
-        this.listContact =  <ListContact onItemClick={this.onItemClick} deleteOn={true}/>
-        
+    render(){ 
         return (
-            <div>
-                {
-                    this.listContact
-                }
-        </div>
+            <ListContact onItemClick={this.onItemClick} deleteOn={this.state.deleteOn} listContact={this.state.listContact} />
         );
     }
 }
